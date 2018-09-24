f02beae2f87d3e7e8bc7c7a555db3267b2ee852f
Breugel <robbebr@hotmail.com>
2018-09-24 18:47:19 +0200

    Subbytes nog bij gemaakt, Datapad moet opgesplitst worden in controlepad en datapad, controlepad bestaat uit 3 verschillende processen

--- Datapad.vhd
+++ Datapad.vhd
@@ -44,11 +44,10 @@ architecture logica of Datapad is
 
 
 
-	component ByteSub is
-   		port( 	BS_in	:in std_logic_vector( 7 downto 0 );
-			BS_out	:out std_logic_vector( 7 downto 0 )
-		);
-	end component; 
+	component Subbytes is 
+ 		port (  p_in : in std_logic_vector(127 downto 0);
+          		s_out : out std_logic_vector(127 downto 0));
+	end
 
 	
 	component ShiftRow is 
@@ -70,23 +69,21 @@ architecture logica of Datapad is
 	signal round_counter: std_logic_vector(3 downto 0); --moeten tot 9 kunnen tellen
 	signal FSM: FSM_type;
 	signal chip_enable: std_logic;
-	
-
 	--/signals
+
+
 
 begin	-- Begin van de "logica"
 
-DutyCycle: process(clk, reset)
+DutyCycle: process(rising_edge (clk))
 	begin
 		if reset = '0' then
 			chip_enable		<= '0';
-			--FSM	 	<= initialize;        ik krijg hier een error van multiple sources? hoe kan ik dat opl?
-			-- moet dit commando hier niet staan in de reset?
 			round_counter   <= (others => '0');
 			Data_out 	<= (others => '0');
 			done		<= '0';
 		elsif rising_edge (clk) then
-			if ce ='1' 
+			if (ce ='1') 
 			then chip_enable <= '1';
 			else chip_enable <= '0';
 			end if;
@@ -96,38 +93,40 @@ end process;
 
 
 
- MainProgram: process(chip_enable)
-begin
-	if chip_enable = '1' then	
-		case FSM is
-			when initialize => 
-				FSM <= first_round;
-				--	
-				--
-				--
-			when first_round =>
-				round_counter <= round_counter + 1;
-				-- De vershillende blokken moeten hier nog in
-				FSM <= loop_till_nine;
-	
-			when loop_till_nine =>
-				if(round_counter < 10 and round_counter >= 1 ) then
-					round_counter <= round_counter + 1;
-				-- De vershillende blokken moeten hier nog in
-				else 
-					round_counter <= (others => '0');
-					FSM <= out_loop;
-				-- De vershillende blokken moeten hier nog in
-				end if;
-	
-			when out_loop =>
-				FSM		<= initialize;
-				done		<= '1';
-				chip_enable	<= '0';
-		
-			end case;
-	end if;
-end process;
+
+-- MainProgram: process(rising_edge (clk))
+--begin
+--	if chip_enable = '1' then	
+--		case FSM is
+--			when initialize => 
+--				FSM <= first_round;
+--				round_counter <= (others => '0');
+--				--	
+--				--
+--				--
+--			when first_round =>
+--				round_counter <= round_counter + 1;
+--				-- De vershillende blokken moeten hier nog in
+--				FSM <= loop_till_nine;
+--	
+--			when loop_till_nine =>
+--				if(round_counter < 10 and round_counter >= 1 ) then
+--					round_counter <= round_counter + 1;
+--				-- De vershillende blokken moeten hier nog in
+--				else 
+--					round_counter <= (others => '0');
+--					FSM <= out_loop;
+--				-- De vershillende blokken moeten hier nog in
+--				end if;
+--	
+--			when out_loop =>
+--				FSM		<= initialize;
+--				done		<= '1';
+--				chip_enable	<= '0';
+--		
+--			end case;
+--	end if;
+--end process;
 
 
 end logica;
